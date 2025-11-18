a6f3ffc2d66244849ba0def57982f3428d58a642
zijyut <zijyut9@gmail.com>
2025-11-19 02:52:34 +0800

    Update import.sql

--- db/scripts/import.sql
+++ db/scripts/import.sql
@@ -2,12 +2,12 @@ PRAGMA foreign_keys = ON;
 
 .read db/migrations/V001__core_init.sql
 .read db/migrations/V002__lab.sql
-
+.read db/migrations/V003__imaging.sql
 
 -- 后续模块追加：
 -- .read db/migrations/V002__lab.sql
--- .read db/migrations/V003__observation.sql
--- .read db/migrations/V004__imaging.sql
+-- .read db/migrations/V003__imaging.sql
+-- .read db/migrations/V004__orders.sql
 -- .read db/migrations/V005__emr_document.sql
 -- .read db/migrations/V006__path_anesthesia.sql
 
